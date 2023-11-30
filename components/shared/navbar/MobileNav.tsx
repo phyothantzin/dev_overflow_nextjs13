79fa5b6a6c1a5f0753d79b973afc8485ef1e1da0
phyothantzin <phyothantzin.w@gmail.com>
2023-11-30 11:47:16 +0630

    implement sidebar

--- components/shared/navbar/MobileNav.tsx
+++ components/shared/navbar/MobileNav.tsx
@@ -64,7 +64,7 @@ const MobileNav = () => {
       </SheetTrigger>
       <SheetContent
         side="left"
-        className="background-light900_dark200 border-none"
+        className="background-light900_dark200 custom-scrollbar overflow-y-auto border-none"
       >
         <Link href="/" className="flex items-center gap-1">
           <Image
@@ -88,14 +88,14 @@ const MobileNav = () => {
               <SheetClose asChild>
                 <Link href="/sign-in">
                   <Button className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
-                    <span className="primary-text-gradient">Login</span>
+                    <span className="primary-text-gradient">Sign In</span>
                   </Button>
                 </Link>
               </SheetClose>
               <SheetClose asChild>
                 <Link href="/sign-up">
                   <Button className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
-                    Register
+                    Sign Up
                   </Button>
                 </Link>
               </SheetClose>
--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -38,7 +38,7 @@ const Navbar = () => {
             }}
           />
         </SignedIn>
-        
+
         <MobileNav />
       </div>
     </nav>
