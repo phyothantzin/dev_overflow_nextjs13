9e83e4adff897b7e8a90db78aa53218a350f4af4
phyothantzin <phyothantzin.w@gmail.com>
2023-12-04 11:25:42 +0630

    added tag remove function

--- components/Forms/Question.tsx
+++ components/Forms/Question.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { Editor } from "@tinymce/tinymce-react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
@@ -18,9 +18,13 @@ import {
 import { Input } from "@/components/ui/input";
 import { QuestionFormSchema } from "@/lib/validations";
 import { Badge } from "../ui/badge";
+import Image from "next/image";
+
+const type: any = "create";
 
 const Question = () => {
   const editorRef = useRef(null);
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
   const form = useForm<z.infer<typeof QuestionFormSchema>>({
     resolver: zodResolver(QuestionFormSchema),
@@ -60,8 +64,20 @@ const Question = () => {
     }
   };
 
+  const handleTagRemove = (tag: string, field: any) => {
+    const newTags = field.value.filter((t: string) => t !== tag);
+    form.setValue("tags", newTags);
+  };
+
   function onSubmit(values: z.infer<typeof QuestionFormSchema>) {
-    console.log(values);
+    setIsSubmitting(true);
+
+    // try {
+
+    // } catch (error) {
+    // } finally {
+    //   setIsSubmitting(false);
+    // }
   }
 
   return (
@@ -161,19 +177,35 @@ const Question = () => {
                 <FormMessage className="text-red-500" />
               </FormLabel>
               <FormControl className="mt-3.5">
-                <Input
-                  className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border"
-                  placeholder="Add tags..."
-                  onKeyDown={(e) => handleInputKeyDown(e, field)}
-                />
+                <>
+                  <Input
+                    className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border"
+                    placeholder="Add tags..."
+                    onKeyDown={(e) => handleInputKeyDown(e, field)}
+                  />
+
+                  {field.value.length > 0 && (
+                    <div className="flex-start mt-2.5 gap-2.5">
+                      {field.value.map((tag: any) => (
+                        <Badge
+                          key={tag}
+                          className="body-medium background-light800_dark300 text-light400_light500 flex items-center justify-center gap-2 rounded-md border-none px-4 py-2 capitalize"
+                          onClick={() => handleTagRemove(tag, field)}
+                        >
+                          {tag}
+                          <Image
+                            src="/assets/icons/close.svg"
+                            alt="close icon"
+                            width={12}
+                            height={12}
+                            className="cursor-pointer object-contain invert-0 dark:invert"
+                          />
+                        </Badge>
+                      ))}
+                    </div>
+                  )}
+                </>
               </FormControl>
-              {field.value.length > 0 && (
-                <div className="flex-start mt-2.5 gap-2.5">
-                  {field.value.map((tag: any) => (
-                    <Badge key={tag}>{tag}</Badge>
-                  ))}
-                </div>
-              )}
               <FormDescription className="body-regular mt-2.5 text-light-500">
                 Add up to 3 tags to describe what your question is about. You
                 need to press enter to add a tag.
@@ -181,7 +213,17 @@ const Question = () => {
             </FormItem>
           )}
         />
-        <Button type="submit">Submit</Button>
+        <Button
+          type="submit"
+          className="primary-gradient w-fit !text-light-900"
+          disabled={isSubmitting}
+        >
+          {isSubmitting ? (
+            <>{type === "edit" ? "Editing..." : "Posting..."}</>
+          ) : (
+            <>{type === "edit" ? "Edit Question" : "Ask a Question"}</>
+          )}
+        </Button>
       </form>
     </Form>
   );
